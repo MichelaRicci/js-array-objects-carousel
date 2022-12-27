e4c2bee85e4424a9905c440ceee7604de2961116
MichelaRicci <michela.ricci.1991@gmail.com>
2022-12-27 18:39:41 +0100

    logica fixed - sistemare css

--- js/data.js
+++ js/data.js
@@ -1,3 +1,11 @@
+/* Milestone 0:
+Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
+Milestone 1:
+Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
+Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile assieme al suo titolo e testo.
+Milestone 2:
+Aggiungere il "ciclo infinito" del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra. */
+
 
 const data = [
   {
@@ -24,6 +32,89 @@ const data = [
 ];
 
 
-// Prendo elemento dalla pagina
 
-const carousel = document.getElementById('carousel');
+// Prendo elementi dalla pagina
+
+const gallery = document.querySelector('#carousel #gallery');
+
+
+//Creo le card
+
+let card = '';
+
+
+// Inietto elementi array dentro HTML 
+
+for (let i = 0; i < data.length; i++) {
+
+  const image = `<img src="${data[i].image}" alt="img"></img>`;
+  
+
+  card += `${image}
+  
+      <div class="text">
+        <h1>"${data[i].title}"</h1>
+        <h3>"${data[i].text}"</h3>
+      </div>`;
+}
+
+
+// Stampo in pagina 
+gallery.innerHTML = card; 
+
+
+
+const img = document.querySelectorAll("#gallery img");
+const txt = document.querySelectorAll("#gallery .text");
+
+
+// Rendo active la prima immagine e i testi
+let currentActiveIndex = 0;
+
+img[currentActiveIndex].classList.add('active');
+txt[currentActiveIndex].classList.add('active');
+
+
+
+
+// Buttons
+
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+
+// FUNCTIONS -------------------------------------------------------------
+
+prev.addEventListener('click', function(){
+
+  img[currentActiveIndex].classList.remove('active');
+  txt[currentActiveIndex].classList.remove('active');
+
+  currentActiveIndex --;
+
+  if (currentActiveIndex < 0) currentActiveIndex = img.length - 1;
+
+  img[currentActiveIndex].classList.add('active');
+  txt[currentActiveIndex].classList.add('active');
+});
+
+next.addEventListener('click', function(){
+
+  img[currentActiveIndex].classList.remove('active');
+  txt[currentActiveIndex].classList.remove('active');
+
+  currentActiveIndex ++;
+
+  if (currentActiveIndex === img.length) currentActiveIndex = 0;
+
+  img[currentActiveIndex].classList.add('active');
+  txt[currentActiveIndex].classList.add('active');
+});
+
+
+
+
+
+
+
+
